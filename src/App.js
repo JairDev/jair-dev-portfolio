ec934e9299202b056f3939945ebc29983490c4ae
Alfredo <devalfredomoscoso@gmail.com>
2020-10-17 16:01:48 -0400

    Responsive desing

--- src/App.js
+++ src/App.js
@@ -3,23 +3,34 @@ import "./App.css";
 import Home from "pages/Home"
 
 function App() {
+  const date = new Date()
+  const hours = date.getHours()
+  const minutes = date.getMinutes()
+  let pmOrAm = ""
+  if (hours < 12) {
+    pmOrAm = "am"
+  }else if(hours >= 12 && hours <= 24) {
+    pmOrAm = "pm"
+  }
+  const nowDate = `${hours}:${minutes} ${pmOrAm}`
+
   return (
     <div className="App">
       <header className="App-header">
         <div className="App-header-content">
-          <span className="span-hi">Hi</span>
-          <div>
+          <span className="span-hi">Hi, Welcome</span>
+          {/* <div>
             <form>
               <input
                 type="text"
                 placeholder="Please, enter your name..."
               ></input>
             </form>
-          </div>
-          <span className="span-welcome">Welcome</span>
+          </div> */}
+          {/* <span className="span-welcome">Welcome</span> */}
         </div>
         <div className="content-date">
-          <span>4:50 pm</span>
+          <span>{nowDate}</span>
         </div>
       </header>
       <Home/>
