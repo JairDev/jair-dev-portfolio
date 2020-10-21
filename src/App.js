c4d308504d69e74e977ec8392522ccf0253c98e5
Alfredo <devalfredomoscoso@gmail.com>
2020-10-21 16:16:44 -0400

    Added a simple animation with gsap

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React, { useEffect, useRef } from "react";
+import {gsap} from "gsap"
 import "./App.css";
 import Home from "pages/Home"
 import CurrentTime from "components/CurrentTime/CurrentTime"
 
 function App() {
-  let elementRef = useRef(null)
-
+  let border = useRef(null)
+  let hi = useRef(null)
   useEffect(() => {
-    console.log(elementRef)
-    
+    gsap.timeline()
+      .from(border, {duration: 1, scaleX: 0, opacity:0})
+      .from(hi, { opacity:0})
   },[])
 
   return (
     <div className="App">
-      <header ref={header => elementRef = header} className="App-header">
+      <header className="App-header">
+        <span ref={span => border = span} className="border-animate"></span>
         <div className="App-header-content">
-          <span className="span-hi">Hi, Welcome</span>
+          <span ref={span => hi = span} className="span-hi">Hi, Welcome</span>
         </div>
         <CurrentTime/>
       </header>
       <Home/>
-
     </div>
   );
 }
