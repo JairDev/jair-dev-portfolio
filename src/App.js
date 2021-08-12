a71dcdb7377c6b07a1e0846716ea5751a684bfc4
JairDev <devalfredomoscoso@gmail.com>
2021-08-12 13:46:15 -0400

    Production Test

--- src/App.js
+++ src/App.js
@@ -5,7 +5,16 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 import Home from "pages/Home";
 import Layout from "pages/Layout/Layout";
 import Challenge from "components/Challenge/Challenge";
+import "./assets/coin.png";
+import "./assets/desert.png";
+import "./assets/css.svg";
+import "./assets/git.svg";
+import "./assets/html.svg";
+import "./assets/js.svg";
+import "./assets/react.svg";
+import "./assets/sass.svg";
 import "./App.css";
+
 
 gsap.registerPlugin(ScrollTrigger);
 
