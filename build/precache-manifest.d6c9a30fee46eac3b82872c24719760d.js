3029d830a7e666ca9ceaee642c9fcb1f5b8edccf
JairDev <devalfredomoscoso@gmail.com>
2022-07-28 13:30:58 -0400

    Build

---
+++ build/precache-manifest.d6c9a30fee46eac3b82872c24719760d.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68dab6740044c109b195d5a8e83436af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1636eb066bba5b3f0a5d",
+    "url": "/static/css/main.63a0b9cb.chunk.css"
+  },
+  {
+    "revision": "6f2d9ff7653af84f368c",
+    "url": "/static/js/2.e4c11528.chunk.js"
+  },
+  {
+    "revision": "76046ff0a0c23daeea99549d8fdd6d87",
+    "url": "/static/js/2.e4c11528.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1636eb066bba5b3f0a5d",
+    "url": "/static/js/main.a6f42222.chunk.js"
+  },
+  {
+    "revision": "55d52d96087d10ab93bc",
+    "url": "/static/js/runtime-main.4893279b.js"
+  },
+  {
+    "revision": "e7a2351eb0e70644d4b74626eca071b1",
+    "url": "/static/media/aerolab-screen.e7a2351e.png"
+  },
+  {
+    "revision": "784c123c8d465d1871cb912194bc6a58",
+    "url": "/static/media/arrow.784c123c.svg"
+  },
+  {
+    "revision": "1fd1b54cc912a1b0c1cd52fab88936b4",
+    "url": "/static/media/blob.1fd1b54c.svg"
+  },
+  {
+    "revision": "93c113d6be110866ad3787b33e7f20ea",
+    "url": "/static/media/coin-mac.93c113d6.webp"
+  },
+  {
+    "revision": "4f65305254bfe3330d30e5f3c5b3ff2c",
+    "url": "/static/media/desert-mac.4f653052.webp"
+  },
+  {
+    "revision": "c4723b9f3b6e7ff6aedb97e2938adc05",
+    "url": "/static/media/hero-phone5.c4723b9f.png"
+  },
+  {
+    "revision": "5eb2cf93ce9901f1ee73f61e54ab1a2f",
+    "url": "/static/media/memory-mac.5eb2cf93.webp"
+  },
+  {
+    "revision": "43bee77099e6699877e3edcd9d94fe86",
+    "url": "/static/media/nav-bar-close.43bee770.svg"
+  },
+  {
+    "revision": "f396aee046ff92b2dd6c5dfe16f219f8",
+    "url": "/static/media/nav-bar-icon.f396aee0.svg"
+  },
+  {
+    "revision": "47263f7f2018923eb30d1f9b0f491f36",
+    "url": "/static/media/room-mac.47263f7f.webp"
+  },
+  {
+    "revision": "08fbb6ffce1817c336497f2cf7f3f02f",
+    "url": "/static/media/timer-mac.08fbb6ff.webp"
+  },
+  {
+    "revision": "5d3f6f3ae4b6b44fb7933d30c791e963",
+    "url": "/static/media/todo-mac.5d3f6f3a.webp"
+  }
+]);
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,23 +2,15 @@ import FormContact from "components/FormContact/FormContact";
 import React from "react";
 import styles from "./Footer.module.css";
 
-import { motion } from "framer-motion/dist/framer-motion";
-
 function Footer() {
   return (
     <section
       id="contact"
       className={`${styles.wrapperPadding} ${styles.contact}`}
     >
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{delay: 1}}
-        exit={{ opacity: 0 }}
-        className={styles.wrapperMaxWidth}
-      >
+      <div className={styles.wrapperMaxWidth}>
         <FormContact />
-      </motion.div>
+      </div>
     </section>
   );
 }
